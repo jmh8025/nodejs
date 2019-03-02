5acf79aeef7f096d77672f1d564a59ce508130cf
jeonminho <jmh8025@nate.com>
2019-03-02 11:06:21 +0900

    0302

--- minho/third/ex03_carlist.js
+++ minho/third/ex03_carlist.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var path = require('path');
+var static = require('serve-static');
+var router = express.Router();
+
 app.set('port',3000);
+app.use('/public',static(path.join('public')));
+
+
+router.route('/car_list').get((req,res)=>{
+    console.log('car_list요청됨');
+});
+
+app.use('/',router);
 
 
 var server = http.createServer(app);

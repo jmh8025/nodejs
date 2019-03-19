666a634cb017afc514c4771689f88eb5129717be
error-404 <jjjmnio@naver.com>
2019-03-19 10:52:14 +0900

    d

---
+++ minho/self/33/src/components/AsyncSplitMe.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class AsyncSplitMe extends Component {
+  state = {
+    
+  }
+}
--- minho/self/react-router-tuto/src/Root.js
+++ minho/self/react-router-tuto/src/Root.js
@@ -3,11 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const Root = () => {
-    return (
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  );
+};
 
 export default Root;
